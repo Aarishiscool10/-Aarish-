b2e1fd3e4672d361ed34fedb7c58903478d8091d
Aarishiscool10 <smile4snehal@gmail.com>
2026-04-23 16:39:27 +1000

    Add files via upload

---
+++ Numberchecker.py
@@ -0,0 +1,11 @@
+number=-int(input ("enter any number and I will do something very cool"))
+
+if number >0:
+ print("Your number is pooosiiitive")
+
+elif number <0:
+ print("Your number is negaaaaativeeeeee")
+
+
+else:
+ print("You picked the special number in this input which is neutral")
---
+++ oddoreven.py
@@ -0,0 +1,11 @@
+number=-int(input ("enter any number and I will do something very cool"))
+
+if number%2==0:
+ print("even numbrrrrr")
+
+elif number==11:
+ print("How did you guess the special number")
+
+else:
+ print("Dats and odddd nuymbrrrrr")
+
