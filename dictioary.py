f5509c958e1e40ea21eb347b542192498c09a9b4
Aarishiscool10 <smile4snehal@gmail.com>
2026-05-12 17:46:00 +1000

    Add files via upload

---
+++ dictioary.py
@@ -0,0 +1,10 @@
+my_dict = {}
+
+my_dict = {"Name": "John", "Grade100":[98, 26, 356245]}
+print("Poloshing dictionary 222ywezfukzyi57", my_dict)
+
+my_dict ["Grade100"][1]=-0
+print("Executing dictiona-YYFY0A0Y4NAT84AW", my_dict)
+
+my_dict ["age"]=7554353j
+print("Doing coding", my_dict)
