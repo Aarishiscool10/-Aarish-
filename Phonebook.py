6a7ba502ff4898837002fbc1db89cadd9e0b6870
Aarishiscool10 <smile4snehal@gmail.com>
2026-05-21 16:46:03 +1000

    Add files via upload

--- Phonebook.py
+++ Phonebook.py
@@ -13,10 +13,29 @@ def initial_phonebook():
         print("/nEnter contacts %d details in the following order (ONLY):" % (i+1))
         print("NOTE * Indicates Mandatory Field")
 
-        print(".../............/......./...LOADING NEXT SECTION......../......../.............../............../.........../.............../........./...")
+        print(".../............/......./...LOADING NEXT SECTOR......../......../.............../............../.........../.............../........./...")
         temp = []
         for j in range(cols):
 
-
-           if j == 0:
+         if j == 0:
                temp.append(str(input("Enter name*: ")))
+
+
+
+               if temp[j] == ' ' or temp[j] == '  ':
+                   sys.exit("Name is mandatory to go on to next sector")
+
+
+         if j == 1:
+               temp.append(str(input("Enter phone number*: ")))
+              
+               if temp[j] == ' ' or temp[j] == '  ':
+                   sys.exit("Phone number is mandatory to go on to next sector")
+
+
+         if j == 2:
+               temp.append(str(input("Enter email*: ")))
+              
+               if temp[j] == ' ' or temp[j] == '  ':
+                   sys.exit("email is mandatory to go on to next sector")
+
